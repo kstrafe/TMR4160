44ba027bf5b88540e0e92b9213911aaf17d7dffc
BourgondAries <macocio@gmail.com>
2016-01-25 23:26:26 +0100

    Autogenerate the pdf file containing the plot.

--- oeving1/generate.sh
+++ oeving1/generate.sh
@@ -1,2 +1,3 @@
 echo "-10\n10\n10000" | ./ex1 > file.dat
-echo "plot \"file.dat\"\npause 10" | gnuplot
+# Part 2: 3)
+echo "set terminal latex\nset out 'plot.tex'\nset key right top\nplot \"file.dat\" title \"\$x^2 sin(\\\\pi x)\$\"" | gnuplot
