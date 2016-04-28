733b636fcdfb596e801a7f132617c78b432903e2
Bourgond Aries <macocio@gmail.com>
2016-04-28 14:31:15 +0200

    Automatically include all code except main2.f

---
+++ createlisting.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+interpolate() {
+	caption=$(echo $1 | sed 's/\_/\\_/g')
+	echo "\\lstinputlisting[caption={${caption##./}},breaklines=true,frame=single,language="$2"]{"$3"}"
+}
+
+for extension in sh f awk; do
+	for filename in $(find -name '*.'"$extension"); do
+		if [ "$filename" != "./main2.f" ]; then
+			interpolate "$filename" bash "$filename"
+		fi
+	done
+done
--- getcode.sh
+++ getcode.sh
@@ -5,7 +5,7 @@ begin_line=$2
 end_line=$3
 
 if [ $# -gt 2 ]; then
-	cat $filename | head -n $end_line | head -n $(($end_line - $begin_line)) \
+	cat $filename | head -n $end_line | tail -n $(($end_line - $begin_line)) \
 		| sed "s/\t/  /g"
 else
 	cat $filename | sed "s/\t/  /g"
