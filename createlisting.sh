28d76b743e64a72b5c255ca3e69b8d05b3401cd3
Bourgond Aries <macocio@gmail.com>
2016-05-03 13:53:06 +0200

    Ensure the correct language is used

--- createlisting.sh
+++ createlisting.sh
@@ -1,16 +1,21 @@
 #! /bin/bash
 
 # Genererer LaTeX listing slik at alt av den inkluderte koden blir autogenerert
+
 interpolate() {
 	caption=$(echo $1 | sed 's/\_/\\_/g')
 	echo "\\lstinputlisting[caption={${caption##./}},breaklines=true,frame=single,language="$2"]{"$3"}"
 }
 
+language=( bash fortran matlab awk bash )
+language_iter=0
+
 for extension in sh f m awk gnuplot; do
 	for filename in $(find -name '*.'"$extension"); do
 		if [ "$filename" != "./main2.f" ]; then
-			interpolate "$filename" bash "$filename"
+			interpolate "$filename" "${language[$language_iter]}" "$filename"
 		fi
 	done
+	$((++language_iter))
 done
 interpolate makefile bash "makefile"
