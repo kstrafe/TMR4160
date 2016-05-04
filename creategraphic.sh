3610ada52b3afd4c5b3dfcab9b7f94c96e7a62a1
Bourgond Aries <macocio@gmail.com>
2016-05-04 07:12:44 +0200

    Add improvements and comment

--- creategraphic.sh
+++ creategraphic.sh
@@ -4,7 +4,11 @@
 
 for i in generated/*.png; do
 	echo '\begin{center}'
-	echo '\includegraphics[width=7in]{'${i}'}'
+	echo '\includegraphics[width=7in]{'"$i"'}'
 	echo "$i" | sed 's/_/\\textunderscore /g'
+	if [ -f "${i%.png}".comment ]; then
+		printf '\n'
+		cat "${i%.png}".comment
+	fi
 	echo '\end{center}'
 done
