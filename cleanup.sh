180942c3b1f0f40028fc8150457b0c684064aa67
Bourgond Aries <macocio@gmail.com>
2016-05-05 21:10:34 +0200

    Remove filter branch

--- cleanup.sh
+++ cleanup.sh
@@ -17,5 +17,4 @@ done
 
 rm *.lol *.pdf *.toc *.aux *.log main2.f 2>/dev/null
 rm report1.tex 2>/dev/null
-git filter-branch --index-filter 'git update-index --remove $(find -name "*.png")' 8eae723464da6362414bec890600f417fbe45ef4 HEAD
 true
